e10ef6aeaea941b57b62c8938ab259a818be47f8
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2023-10-10 10:50:34 -0300

    feat: First Commit
    First Commit

---
+++ AppListaCurso/app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "devandroid.vinicius.applistacurso"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "devandroid.vinicius.applistacurso"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
