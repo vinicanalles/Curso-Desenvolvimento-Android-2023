89d2d8c21efa59ee1af80e2485fb375128e70348
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2023-10-14 13:13:19 -0300

    feat: trabalhando com editText
    - Recuperando elementos da tela através do método findViewById;
    - Definindo um texto no campo de editText através do método setText, passando como parâmetro o atributo de uma variável do objeto Pessoa;

--- AppListaCurso/app/src/main/java/devandroid/vinicius/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/vinicius/applistacurso/view/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.vinicius.applistacurso.R;
 import devandroid.vinicius.applistacurso.model.Pessoa;
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         pessoa = new Pessoa();
         outraPessoa = new Pessoa();
 
+        EditText editPrimeiroNome;
+        EditText editSobrenome;
+        EditText editNomeCurso;
+        EditText editTelefoneContato;
+
+        Button btnLimpar;
+        Button btnSalvar;
+        Button btnFinalizar;
+
         // Atribuir conteúdo, dados, valores para o Objeto
         pessoa.setPrimeiroNome("Vinicius");
         pessoa.setSobrenome("Canalles");
@@ -36,23 +47,19 @@ public class MainActivity extends AppCompatActivity {
         outraPessoa.setCursoDesejado("Android");
         outraPessoa.setTelefone("16997646368");
 
-//        dadosPessoa = "Primeiro nome: ";
-//        dadosPessoa += pessoa.getPrimeiroNome();
-//        dadosPessoa += " Sobrenome: ";
-//        dadosPessoa += pessoa.getSobrenome();
-//        dadosPessoa += " Curso Desejado: ";
-//        dadosPessoa += pessoa.getCursoDesejado();
-//        dadosPessoa += " Telefone: ";
-//        dadosPessoa += pessoa.getTelefone();
-//
-//        dadosOutraPessoa = "Primeiro nome: ";
-//        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
-//        dadosOutraPessoa += " Sobrenome: ";
-//        dadosOutraPessoa += outraPessoa.getSobrenome();
-//        dadosOutraPessoa += " Curso Desejado: ";
-//        dadosOutraPessoa += outraPessoa.getCursoDesejado();
-//        dadosOutraPessoa += " Telefone: ";
-//        dadosOutraPessoa += outraPessoa.getTelefone();
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        editPrimeiroNome.setText(outraPessoa.getPrimeiroNome());
+        editSobrenome.setText(outraPessoa.getSobrenome());
+        editNomeCurso.setText(outraPessoa.getCursoDesejado());
+        editTelefoneContato.setText(outraPessoa.getTelefone());
 
         Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
         Log.i("POOAndroid", "Objeto outraPessoa: " + outraPessoa.toString());
