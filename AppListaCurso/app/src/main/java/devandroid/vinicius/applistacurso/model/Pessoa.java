2539f622bd24c1e25fc1e6362107c3a51835e44a
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2023-10-14 12:33:38 -0300

    feat: métodos get e set
    - Uso dos métodos Get e Set;

--- AppListaCurso/app/src/main/java/devandroid/vinicius/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/vinicius/applistacurso/model/Pessoa.java
@@ -1,4 +1,48 @@
 package devandroid.vinicius.applistacurso.model;
 
 public class Pessoa {
+
+    //Construtor
+    public Pessoa() {
+
+    }
+
+    //Atributos
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefone;
+
+    //Métodos de Acesso - Gets and Sets
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- AppListaCurso/app/src/main/java/devandroid/vinicius/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/vinicius/applistacurso/view/MainActivity.java
@@ -11,11 +11,46 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
 
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        outraPessoa = new Pessoa();
+
+        // Atribuir conteúdo, dados, valores para o Objeto
+        pessoa.setPrimeiroNome("Vinicius");
+        pessoa.setSobrenome("Canalles");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefone("16997711804");
+
+        outraPessoa.setPrimeiroNome("Renan");
+        outraPessoa.setSobrenome("Canalles");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setTelefone("16997646368");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone: ";
+        dadosPessoa += pessoa.getTelefone();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobrenome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone: ";
+        dadosOutraPessoa += outraPessoa.getTelefone();
     }
 }
